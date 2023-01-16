8ee363834f6ba7a6864bdb49cd08c071e3e6b209
Scott <scottqglobal@gmail.com>
2023-01-16 14:34:29 -0800

    Updated copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="mt-8 mb-4 md:mt-auto text-left text-sm opacity-40">
-            @ 2022 Scott Quach - Inspired by Takuya Matsuyama and Adam Whitcroft
+            @ 2023 Scott Quach - Design inspired by Takuya Matsuyama and Adam Whitcroft
         </div>
     );
 }
