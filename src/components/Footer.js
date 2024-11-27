d67c9f1228c665bb4daec43f1e570654b4d559ea
Scott <scottqglobal@gmail.com>
2024-11-26 22:35:57 -0800

    Made footer use dynamic year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
+import { useMemo } from "react";
+
 export default function Footer() {
+
+
+    const year = useMemo(() => new Date().getFullYear(), []);
+
     return (
         <div className="mt-8 mb-4 md:mt-auto text-left text-sm opacity-40">
-            @ 2023 Scott Quach - Design inspired by Takuya Matsuyama and Adam Whitcroft
+            @ {year} Scott Quach - Design inspired by Takuya Matsuyama and Adam Whitcroft
         </div>
     );
 }
